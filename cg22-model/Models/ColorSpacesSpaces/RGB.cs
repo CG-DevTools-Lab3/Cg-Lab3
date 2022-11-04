38fd95258ca8a146b9e30785256c884199ea8463
lievvl <37303364+lievvl@users.noreply.github.com>
2022-11-04 22:38:30 +0300

    feat: added color spaces
    #CGDTL3-2

---
+++ cg22-model/Models/ColorSpacesSpaces/RGB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cg22_model.Models.ColorSpacesSpaces
+{
+    public class RGB : IColorSpace
+    {
+        public FloatPixel[,] ToRGB(FloatPixel[,] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FloatPixel[,] FromRGB(FloatPixel[,] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FloatPixel[,] ScaleFrom256(FloatPixel[,] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public FloatPixel[,] ScaleTo256(FloatPixel[,] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Check(FloatPixel[,] image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetComponentsNames()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
